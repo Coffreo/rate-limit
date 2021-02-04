9a7c11d2c76ebd291f2901f9a58da74ed3ae8377
Grenier jérémie <jgrenier@coffreo.com>
2021-02-04 11:53:39 +0100

    feat(predis rate limiter): implement 'RateLimiterStatus' interface

--- src/PredisRateLimiter.php
+++ src/PredisRateLimiter.php
@@ -10,7 +10,7 @@ use function ceil;
 use function max;
 use function time;
 
-final class PredisRateLimiter implements RateLimiter, SilentRateLimiter
+final class PredisRateLimiter implements RateLimiter, SilentRateLimiter, RateLimiterStatus
 {
     /** @var ClientInterface */
     private $predis;
@@ -46,6 +46,20 @@ final class PredisRateLimiter implements RateLimiter, SilentRateLimiter
         if ($current <= $rate->getOperations()) {
             $current = $this->updateCounter($key, $rate->getInterval());
         }
+
+        return Status::from(
+            $identifier,
+            $current,
+            $rate->getOperations(),
+            time() + $this->ttl($key)
+        );
+    }
+
+    public function getRateLimitStatus(string $identifier, Rate $rate): Status
+    {
+        $key = $this->key($identifier, $rate->getInterval());
+
+        $current = $this->getCurrent($key) ;
 
         return Status::from(
             $identifier,
